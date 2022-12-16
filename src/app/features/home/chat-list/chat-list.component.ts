07e0a68b269ef997b116829f282d4aa0075fc051
Hemanth Thallapaneni <tallapaneni.hemanth@gmail.com>
2022-12-16 01:12:29 -0500

    Design changes

--- src/app/features/home/chat-list/chat-list.component.ts
+++ src/app/features/home/chat-list/chat-list.component.ts
@@ -1,74 +1,77 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { AuthServiceService } from 'src/app/auth/auth-service.service';
 import { ChatServiceService } from 'src/app/core/chat-service.service';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-chat-list',
   templateUrl: './chat-list.component.html',
-  styleUrls: ['./chat-list.component.scss']
+  styleUrls: ['./chat-list.component.scss'],
 })
 export class ChatListComponent implements OnInit {
-
-  cards:any = [];
+  cards: any = [];
+  selectedIndex: number;
   constructor(
     private chatservice: ChatServiceService,
-    private auth:AuthServiceService,
-    public dialog: MatDialog,
-    
-  ) { }
+    private auth: AuthServiceService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-   this.getChatsList();
+    this.getChatsList();
   }
-  async getChatsList(){
+  async getChatsList() {
     this.cards = await this.chatservice.getChats();
-    console.log('cards',this.cards)
+    console.log('cards', this.cards);
   }
-  creatChat(){
+  creatChat() {
     const dialogRef = this.dialog.open(GroupDetailsDialog, {
       width: '250px',
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-    
+    dialogRef.afterClosed().subscribe((result) => {
       this.chatservice.create(result);
-     
     });
-    
+  }
+  calculate_last_seen(date: Date) {
+    let last_day = date;
+    let current_day = new Date();
+    var timeDiff = Math.abs(last_day.getTime() - current_day.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    // var diffmonths = Math.ceil(timeDiff / (1000 * 3600 * 24 * 12));
+    return diffDays;
   }
 
-   
-  
-  signOut(){
-    this.auth.signOut()
+  signOut() {
+    this.auth.signOut();
   }
-  selectChat(card:any){
+  setIndex(i: number) {
+    this.selectedIndex = i;
+  }
+  selectChat(card: any) {
     this.chatservice.chatId.next(card);
-   
   }
-
 }
 @Component({
   selector: 'group-details-dialog',
   templateUrl: 'group-details-dialog.html',
-  styleUrls: ['./chat-list.component.scss']
+  styleUrls: ['./chat-list.component.scss'],
 })
 export class GroupDetailsDialog {
-  path:string;
+  path: string;
   group = {
-    name:'',
-    image:''
-  }
-  constructor(
-    public dialogRef: MatDialogRef<GroupDetailsDialog>,
-  ) {}
+    name: '',
+    image: '',
+  };
+  constructor(public dialogRef: MatDialogRef<GroupDetailsDialog>) {}
 
-  upload(event:any){
-     this.group.image = event?.target.files[0]
+  upload(event: any) {
+    this.group.image = event?.target.files[0];
   }
-
 
   onNoClick(): void {
     this.dialogRef.close();
